7aa7f153f9d7296a165c1c126805a3cbc6ccc7f4
acallem <adolfodelacalle@hotmail.com>
2020-12-03 12:31:22 +0100

    no message

--- despliegue.sh
+++ despliegue.sh
@@ -5,6 +5,13 @@
 # imagen base openshift/redhat-openjdk18-openshift:1.5
 # new-app con --as-deployment-config
 
+inicio_tarea()
+{
+	read -rsp $'PULSA UNA TECLA PARA CONTINUAR...\n' -n1 key
+	clear
+	echo $
+}
+
 NAMESPACE_ACOC="acoc-desa-bck"
 NAMESPACE_DESTINO="tlc1-desa-bck"
 NAMESPACE_COMPILACION="imgb-desa-bck"
@@ -17,71 +24,65 @@ FICHERO="./rest-service-0.0.1-SNAPSHOT.jar"
 IMAGEN_BASE="openshift/redhat-openjdk18-openshift:1.5"
 
 #CREACION ENTORNO
-echo CREACION PROYECTOS
+inicio_tarea CREACION NAMESPACES
 oc new-project ${NAMESPACE_ACOC}
 oc new-project ${NAMESPACE_DESTINO}
 oc new-project ${NAMESPACE_COMPILACION}
 
-read -rsp $'Press any key to continue...\n' -n1 key
-
-echo CREACION TEMPLATES
+inicio_tarea CREACION TEMPLATES
 oc create -f template-configmap-tapv4-config-v1.yml -n ${NAMESPACE_ACOC}
 oc create -f template-pvc-telco-nas-v1.yml -n ${NAMESPACE_ACOC}
 oc create -f template-srv-nuc-jee-v4.yml -n ${NAMESPACE_ACOC}
 oc create -f template-srv-nuc-jee-nas-v4.yml -n ${NAMESPACE_ACOC}
 
-read -rsp $'Press any key to continue...\n' -n1 key
-
-echo EJECUCION TEMPLATES
+inicio_tarea INICIALIZACION NAMESPACES
 oc process template-configmap-tapv4-config-v1 -n ${NAMESPACE_ACOC}|oc create -n ${NAMESPACE_DESTINO} -f -
 
 #JENKINS
-echo COMPROBACION BUILD
+inicio_tarea COMPROBACION BUILD
 oc get istag ${APLICACION}:${VERSION} -n ${NAMESPACE_DESTINO}
 oc get bc ${APLICACION} -n ${NAMESPACE_DESTINO}
-echo CREACION BUILD
+
+inicio_tarea CREACION BUILD
 oc new-build --name ${APLICACION} --binary=true --strategy=source --image-stream=${IMAGEN_BASE} --to ${APLICACION}:${VERSION_MAJOR} -n ${NAMESPACE_COMPILACION}
-echo EJECUCION BUILD
+
+inicio_tarea EJECUCION BUILD
 oc start-build ${APLICACION} --from-file=${FICHERO} -n ${NAMESPACE_COMPILACION} --wait=true
-echo TAGEO BUILD
-oc tag ${APLICACION}:${VERSION_MAJOR} ${APLICACION}:${VERSION} -n ${NAMESPACE_COMPILACION}
-echo OBTENCION DETALLES BUILD
+
+inicio_tarea OBTENCION DETALLES BUILD
 oc logs bc/${APLICACION} -n ${NAMESPACE_COMPILACION}
 oc describe bc/${APLICACION} -n ${NAMESPACE_COMPILACION}
 
-read -rsp $'Press any key to continue...\n' -n1 key
+inicio_tarea TAGEO IMAGEN
+oc tag ${APLICACION}:${VERSION_MAJOR} ${APLICACION}:${VERSION} -n ${NAMESPACE_COMPILACION}
 
 #CLARIVE
-echo TAGEO ENTRE ENTORNOS
+inicio_tarea TAGEO IMAGEN ENTRE ENTORNOS
 oc tag ${NAMESPACE_COMPILACION}/${APLICACION}:${VERSION} ${NAMESPACE_DESTINO}/${APLICACION}:${VERSION} ${NAMESPACE_DESTINO}/${APLICACION}:${VERSION_MAJOR}
-echo COMPROBACION DEPLOYMENT
+
+inicio_tarea COMPROBACION DEPLOYMENT
 oc get dc ${APLICACION} -n ${NAMESPACE_DESTINO}
-echo CREACION DEPLOYMENT
+
+inicio_tarea CREACION DEPLOYMENT
 oc new-app --as-deployment-config -e TZ=Europe/Madrid -p APPLICATION_NAME=${APLICACION} -p VERSION_TAG=${VERSION} -p NAMESPACE=${NAMESPACE_DESTINO} -p DOMAIN_SUFFIX=.acme -n ${NAMESPACE_DESTINO} --template ${NAMESPACE_ACOC}/${TEMPLATE}
-echo EJECUCION DEPLOYMENT
+
+inicio_tarea EJECUCION DEPLOYMENT
 oc rollout latest ${APLICACION} -n ${NAMESPACE_DESTINO}
-echo OBTENCION DETALLE DESPLIEGUE
+
+inicio_tarea OBTENCION DETALLE DESPLIEGUE
 oc rollout status dc/${APLICACION} -n ${NAMESPACE_DESTINO}
 
-read -rsp $'Press any key to continue...\n' -n1 key
-
 #CLARIVE PATCH
-echo MODIFICACION DEPLOYMENT
-oc patch dc/${APLICACION} --patch “$(oc process ${TEMPLATE} -p APPLICATION_NAME=${APLICACION} -p VERSION_TAG=${VERSION} -p NAMESPACE=${NAMESPACE_DESTINO} -p DOMAIN_SUFFIX=.acme -p NUM_REPLICAS=2 -n ${NAMESPACE_ACOC} -o json | jq -c ‘.items[] | select(.kind=="DeploymentConfig") | del(.spec.template.spec.containers[].image)’)“ -n ${NAMESPACE_DESTINO}
+inicio_tarea MODIFICACION DEPLOYMENT
+oc patch dc/${APLICACION} --patch "$(oc process ${TEMPLATE} -p APPLICATION_NAME=${APLICACION} -p VERSION_TAG=${VERSION} -p NAMESPACE=${NAMESPACE_DESTINO} -p DOMAIN_SUFFIX=.acme -p NUM_REPLICAS=2 -n ${NAMESPACE_ACOC} -o json | jq -c ‘.items[] | select(.kind=="DeploymentConfig") | del(.spec.template.spec.containers[].image)’)" -n ${NAMESPACE_DESTINO}
 
-read -rsp $'Press any key to continue...\n' -n1 key
-
-echo MODIFICACION VARIABLES
+inicio_tarea MODIFICACION VARIABLES
 oc set env dc/${APLICACION} --overwrite -e TZ=Europe/Madrid -e ACME=77 -n ${NAMESPACE_DESTINO}
 
-read -rsp $'Press any key to continue...\n' -n1 key
-
-echo REDESPLIEGUE
+inicio_tarea REDESPLIEGUE
 oc rollout latest ${APLICACION} -n ${NAMESPACE_DESTINO}
 
-read -rsp $'Press any key to continue...\n' -n1 key
-
-echo ROLLBACK
+inicio_tarea ROLLBACK
 oc rollback ${APLICACION} -n ${NAMESPACE_DESTINO}
 
-echo FIN
+inicio_tarea FIN
